881fdec6f7e3ad02ec5ec86d40b71c370dc7202a
Maurice Williams <morficus@gmail.com>
2014-12-24 18:58:28 -0500

    adding support for `collectionEvents` attribute 	- new private helper function now handles all event binding

--- src/modelEvents.js
+++ src/modelEvents.js
@@ -15,20 +15,34 @@
         // delegate normal events as usual
         original.call(this, events);
 
-        var that = this;
-        _.each( this.modelEvents, function (handler, event) {
-            if (_.isString(handler)) {
-                that.listenTo(that.model, event, that[handler]);
-            } else if (_.isFunction(handler)) {
-                that.listenTo(that.model, event, handler);
-            } else if (_.isArray(handler)) {
-                //TODO: would be neat to make this a recursive function, that way the callback-array is more flexible
-                _.each(handler, function (callback) {
-                    that.listenTo(that.model, event, that[callback]);
-                });
-            }
+        var that = this,
+            bindEvents;
 
-        });
+        // private helper function for handling event binding
+        bindEvents = function (eventHash, entity) {
+            _.each( eventHash, function (handler, event) {
+                if (_.isString(handler)) {
+                    that.listenTo(that[entity], event, that[handler]);
+                } else if (_.isFunction(handler)) {
+                    that.listenTo(that[entity], event, handler);
+                } else if (_.isArray(handler)) {
+                    //TODO: would be neat to make this a recursive function, that way the callback-array is more flexible
+                    _.each(handler, function (callback) {
+                        that.listenTo(that[entity], event, that[callback]);
+                    });
+                }
+            });
+        };
+
+        // automatically bind to model and collection events
+        if (this.modelEvents) {
+            bindEvents(_.result(this, 'modelEvents'), 'model');
+        }
+
+        if (this.collectionEvents) {
+            bindEvents(_.result(this, 'collectionEvents'), 'collection');
+        }
+
 
         return this;
     });
