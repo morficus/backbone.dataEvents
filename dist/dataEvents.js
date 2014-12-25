723058bd327cd5cdeccff7319d453ab45c8edc28
Maurice Williams <morficus@gmail.com>
2014-12-25 17:39:10 -0500

    adding release files for v0.1.0

---
+++ dist/dataEvents.js
@@ -0,0 +1,67 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['backbone', 'underscore'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('backbone'), require('underscore'));
+    } else {
+        factory(window.Backbone, window._);
+    }
+})(function (Backbone, _) {
+
+    var ViewPrototype = Backbone.View.prototype;
+
+    ViewPrototype.delegateEvents = _.wrap(ViewPrototype.delegateEvents, function(original, events) {
+
+        // delegate normal events as usual
+        original.call(this, events);
+
+        var that = this,
+            bindEvents;
+
+        // private helper function for handling event binding
+        bindEvents = function (eventHash, entity) {
+
+            // check that the entity type (model or collection) exists before trying to listen to its events
+            if (!that.hasOwnProperty(entity)) {
+                throw 'No "' + entity + '" defined: You defined a "' + entity + 'Events" hash but your view does not have a "' + entity + '" attribute.';
+            }
+
+            _.each( eventHash, function (handler, event) {
+                if (_.isString(handler)) {
+                    that.listenTo(that[entity], event, that[handler]);
+                } else if (_.isFunction(handler)) {
+                    that.listenTo(that[entity], event, handler);
+                } else if (_.isArray(handler)) {
+                    //TODO: would be neat to make this a recursive function, that way the callback-array is more flexible
+                    _.each(handler, function (callback) {
+                        that.listenTo(that[entity], event, that[callback]);
+                    });
+                }
+            });
+        };
+
+        // automatically bind to model and collection events
+        if (this.modelEvents) {
+            bindEvents(_.result(this, 'modelEvents'), 'model');
+        }
+
+        if (this.collectionEvents) {
+            bindEvents(_.result(this, 'collectionEvents'), 'collection');
+        }
+
+
+        return this;
+    });
+
+    ViewPrototype.undelegateEvents = _.wrap(ViewPrototype.delegateEvents, function(original, events) {
+        original.call(this, events);
+
+        if (this.modelEvents) {
+            this.stopListening(this.model);
+        }
+
+        if (this.collectionEvents) {
+            this.stopListening(this.collection);
+        }
+    });
+});
